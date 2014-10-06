73724b4a119312308f6665d12e01a644213f92b0
Bryan Alves <bryanalves@gmail.com>
2014-10-05 22:34:10 -0400

    Convert erb to haml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def conditional_html(&block)
+    cls = "no-js wf-loading"
+    return <<-HTML.gsub(/^\s+/, '').html_safe + capture(&block) + "</html>".html_safe
+      <!--[if lt IE 9 ]>              <html lang="en" class="#{cls} lt-ie10 lt-ie9"> <![endif]-->
+      <!--[if IE 9 ]>                 <html lang="en" class="#{cls} lt-ie10"> <![endif]-->
+      <!--[if (gte IE 9)|!(IE)]><!--> <html lang="en" class="#{cls}"> <!--<![endif]-->
+    HTML
+  end
 end
