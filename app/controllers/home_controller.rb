5dad6e902b95cd8a4d24be78624146f071e5ffad
Bryan Alves <bryanalves@gmail.com>
2014-10-06 22:29:24 -0400

    Require login to manage users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,4 @@
 class HomeController < ApplicationController
-  skip_before_action :require_login
-
   def index
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class UsersController < AuthorizedController
   def new
     @user = User.new
   end
