d25afe88f7e2bf80b73ba6a90cf15b5f072b5f7f
Bryan Alves <bryanalves@gmail.com>
2014-10-06 23:07:52 -0400

    Make monban work in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+Monban.test_mode!
+
 class ActiveSupport::TestCase
+  include Monban::Test::ControllerHelpers
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
