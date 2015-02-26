457354ef463d37bf22f85b7c9dd06edb4cf14647
Bryan Alves <bryanalves@gmail.com>
2015-02-25 20:46:20 -0500

    Only allow real users to manage things

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, :all if user.user? || user.admin?
+    can :manage, :all if user.persisted? || user.admin?
     cannot :manage, User unless user.admin?
     cannot :manage, Category unless user.admin?
   end
